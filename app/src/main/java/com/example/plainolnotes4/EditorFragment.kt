89baa0aa97bd0be1a25f21e606564424a5c1b8a7
Larry Hunter <larry.hunter@outlook.com>
2021-03-18 21:05:13 -0600

    * Created a second fragment for editing notes.

---
+++ app/src/main/java/com/example/plainolnotes4/EditorFragment.kt
@@ -0,0 +1,25 @@
+package com.example.plainolnotes4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class EditorFragment : Fragment() {
+
+    private lateinit var viewModel: EditorViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.editor_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditorViewModel::class.java)
+    }
+}
