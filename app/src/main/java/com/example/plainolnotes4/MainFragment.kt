2cfc5116395225cbbf723c8aa1770f750ec0182a
Larry Hunter <larry.hunter@outlook.com>
2021-03-28 18:17:29 -0600

    * Added options menu to the main fragment.

--- app/src/main/java/com/example/plainolnotes4/MainFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -2,9 +2,7 @@ package com.example.plainolnotes4
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -25,6 +23,7 @@ class MainFragment : Fragment(),
         savedInstanceState: Bundle?
     ): View {
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        setHasOptionsMenu(true)
 
         binding = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
@@ -45,6 +44,22 @@ class MainFragment : Fragment(),
         })
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sample_data -> addSampleData()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun addSampleData(): Boolean {
+        TODO("Not yet implemented")
     }
 
     override fun onItemClick(noteId: Int) {
